772293d464a62ee9d01fee337aaae02202df71bd
zardon <lmyyah@gmail.com>
2021-12-31 15:56:49 +0800

    feat: CuratorConfiguration

---
+++ seckill-service/src/main/java/org/seckill/config/CuratorConfiguration.java
@@ -0,0 +1,39 @@
+package org.seckill.config;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CuratorConfiguration {
+    // 重试次数
+    @Value("$(zookeeper.retryCount)")
+    private Integer retryCount;
+    // 连接超时时间
+    @Value("$(zookeeper.elapsedTimeMs)")
+    private Integer elapsedTimeMs;
+    // zk集群
+    @Value("$(zookeeper.connectString)")
+    private String connectString;
+    // 会话超时时间
+    @Value("$(zookeeper.sessionTimeoutMs)")
+    private Integer sessionTimeoutMs;
+    // 连接超时时间
+    @Value("$(zookeeper.connectionTimeoutMs)")
+    private Integer connectionTimeoutMs;
+
+    @Bean
+    public CuratorFramework curatorFramework() {
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.builder()
+                .connectString(connectString)
+                .sessionTimeoutMs(sessionTimeoutMs)
+                .connectionTimeoutMs(connectionTimeoutMs)
+                .retryPolicy(new RetryNTimes(retryCount, elapsedTimeMs))
+                .build();
+        curatorFramework.start();
+        return curatorFramework;
+    }
+}
