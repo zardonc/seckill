313dd2fed0fcfd4ea2e677e68a86f844c444cf37
zardon <lmyyah@gmail.com>
2022-01-14 18:00:42 +0800

    test: redistemplate locker

--- seckill-web/src/test/java/RedisTemplateTest.java
+++ seckill-web/src/test/java/RedisTemplateTest.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.util.concurrent.TimeUnit;
+
 @SpringBootTest(classes = SecondKillApplication.class)
 public class RedisTemplateTest {
     @Autowired
@@ -14,5 +16,11 @@ public class RedisTemplateTest {
         // 常用操作在opsFor...方法内
         redisTemplate.opsForValue().set("testKey", "123");
         System.out.println(redisTemplate.opsForValue().get("testKey"));
+        redisTemplate.delete("testKey");
+        // 简单分布式锁
+        Boolean opt = redisTemplate.opsForValue().setIfAbsent("dLock:test-lock", "abc-locker", 5L, TimeUnit.SECONDS);
+        System.out.println(opt);
+        Boolean opt2 = redisTemplate.opsForValue().setIfAbsent("dLock:test-lock", "abc-locker2", 5L, TimeUnit.SECONDS);
+        System.out.println(opt2);
     }
 }
