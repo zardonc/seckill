6519b9b0a18ebbf62380c3b636671051d732241d
zardon <lmyyah@gmail.com>
2022-02-07 22:57:00 +0800

    build: add dep

--- seckill-service/src/main/java/org/seckill/SecKillServiceApplication.java
+++ seckill-service/src/main/java/org/seckill/SecKillServiceApplication.java
@@ -1,5 +1,6 @@
 package org.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@MapperScan("org.seckill.dao")
 public class SecKillServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecKillServiceApplication.class, args);
--- seckill-web/src/main/java/org/seckill/SecondKillApplication.java
+++ seckill-web/src/main/java/org/seckill/SecondKillApplication.java
@@ -1,6 +1,5 @@
 package org.seckill;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableCaching
 @SpringBootApplication
 @EnableAsync(proxyTargetClass = true)
-@MapperScan("org.seckill.dao")
 public class SecondKillApplication {
     public static void main(String[] args) {
         SpringApplication.run(SecondKillApplication.class, args);
