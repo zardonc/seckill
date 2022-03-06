df5ba2b1df211ca0e1668042465974dfaf1baefd
zardon <lmyyah@gmail.com>
2022-03-06 23:18:19 +0800

    feat: product service

--- seckill-service/seckill-product/src/main/java/org/seckill/product/ProductApplication.java
+++ seckill-service/seckill-product/src/main/java/org/seckill/product/ProductApplication.java
@@ -1,5 +1,6 @@
 package org.seckill.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients({"org.seckill"})
 @EnableDiscoveryClient
+@MapperScan({"org.seckill.product.mapper"})
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
