387b0f7b55ba2eb9c54c0838f6885867ace40a5c
zardon <lmyyah@gmail.com>
2022-04-23 16:19:59 +0800

    feat: remove redundant module

--- seckill-web/src/main/java/org/seckill/controller/LoginController.java
+++
@@ -1,9 +0,0 @@
-package org.seckill.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/login")
-public class LoginController {
-}
